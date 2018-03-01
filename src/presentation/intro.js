43b506e4bf52c359ed4e661d7c8b1dc9014b2407
Dustin Schau <dustinschau@gmail.com>
2018-02-28 20:20:21 -0600

    chore: tweak title

--- src/presentation/intro.js
+++ src/presentation/intro.js
@@ -4,10 +4,7 @@ import { Heading, Text } from 'spectacle';
 export const Intro = () => (
   <React.Fragment>
     <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-      Spectacle Boilerplate
+      Frontend Advice
     </Heading>
-    <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-      open the presentation/index.js file to get started
-    </Text>
   </React.Fragment>
 );
